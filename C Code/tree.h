6a0a38caee398e551ca1428bbf1bb2330df145ab
ckallum <in18536@bristol.ac.uk>
2018-11-29 17:46:06 +0000

    h

--- C Code/tree.h
+++ C Code/tree.h
@@ -9,20 +9,6 @@ typedef struct huffNode huffNode;
 struct nodeLists;
 typedef struct nodeLists nodeLists;
 
-huffData *newHeapArr(int capacity, char chars[], int freqs[]);
-huffNode *newNode(char data, int freq);
-nodeLists *newList(int capacity);
-
-void add2List(nodeLists *l, huffNode *n);
-huffNode *subFromList(nodeLists *l);
-
-huffNode *findMin(nodeLists *l1, nodeLists *l2);
-int nodeLeaf(huffNode *n);
-huffNode *huffTree(huffData *array);
-void printCodes(int codeArray[], int parent, huffNode *base);
-void HuffmanCodes(huffData *array);
-
-
 int *getHeapFreq(const int arrLen, int freqs[]);
 char *getHeapData(const int arrLen, char chars[]);
 int testGetData(char input);
