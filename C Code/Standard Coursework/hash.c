e08398830948864b97beb392ec51c775c2b91214
ckallum <in18536@bristol.ac.uk>
2018-12-04 15:56:56 +0000

    hehehheh

--- C Code/Standard Coursework/hash.c
+++ C Code/Standard Coursework/hash.c
@@ -13,7 +13,6 @@ struct Node{
 
 struct HashTable{
   Node **array;
-  int arrayMax;
   int capacity;
   int max;
 };
@@ -25,16 +24,30 @@ Node *newNode(char *data){
   return new;
 }
 
-HashTable *newHash(int max){
+HashTable *newHash(){
   HashTable *h = malloc(sizeof(HashTable));
-  h->array = malloc(max*sizeof(Node));
-  for (int i = 0; i < max; i++){
+  h->max = 8;
+  h->array = malloc(h->max*sizeof(Node));
+  for (int i = 0; i < h->max; i++){
     h->array[i] = NULL;
   }
   h->capacity = 0;
-  h->arrayMax = 8;
-  h->max = 8;
   return h;
+}
+
+void freeHash(HashTable *h){
+  for (int i = 0; i < h->max; i++){
+    if(h->array[i]==NULL) free(h->array[i]);
+    else {
+      Node *temp = h->array[i];
+      Node *next = temp->next;
+      while(temp != NULL){
+        free(temp);
+        temp = next;
+        next = temp->next;
+      }
+    }
+  }
 }
 
 int HashFunction (char *ch, HashTable *h){
@@ -51,23 +64,6 @@ bool isEmpty(HashTable *h, int index){
   return false;
 }
 
-bool HashFull(HashTable *h){
-  if (h->capacity == 8) return true;
-  return false;
-
-}
-
-bool ListFull(HashTable *h, int index){
-  int count = 0;
-  Node *temp = h->array[index];
-  while (temp != NULL){
-      count ++;
-      temp = temp->next;
-  }
-  if (count == 8) return true;
-  return false;
-}
-
 int findKinI(HashTable *h, int index, char *data){
   Node *temp = h->array[index];
   int listIndex = 0;
@@ -79,46 +75,95 @@ int findKinI(HashTable *h, int index, char *data){
   return -1;
 }
 
-void increaseHash(HashTable *h){
-  h->max = h->max*2;
-  h->array = realloc(h->array, h->max);
+Node *getNode(HashTable *h, int listIndex,int index){
+  Node *temp =h->array[index];
+  for (int i = 0 ; i<= listIndex; i++){
+    temp = temp->next;
+  }
+  return temp;
 }
 
-void reHash(HashTable *h){
+bool HashFull(HashTable *h){
+  if ((h->capacity/h->max) >= 0.75){
+    return true;
+  }
+  return false;
+}
 
+
+void reHash(HashTable *h){
+  HashTable *temp = h;
+  int newMax = (h->max) *2;
+  h->max = newMax;
+  h->array = realloc(h->array, h->max * sizeof(Node));
+  for (int i = 0; i < h->max; i++){
+    h->array[i] = NULL;
+  }
+  for (int i = 0; i < temp->max; i++){
+    Node *tempNode = temp->array[i];
+    if (tempNode == NULL) i++;
+    while(tempNode != NULL){
+      insertHash(h, tempNode->data);
+      tempNode = tempNode->next;
+    }
+  }
 }
 
 void insertHash(HashTable *h, char *ch){
   int index = HashFunction(ch, h);
   Node *new = newNode(ch);
-  if (HashFull(h) == true){
-    while(ListFull(h,index) == true){
-      if (index==h->max){
-        increaseHash(h);
-        break;
-      }
-      else index ++;
-    }
-    insertHash(h,ch);
-  }
-  else if (isEmpty(h, index)==true){
+  int listIndex = findKinI(h, index, ch);
+  if (isEmpty(h, index)==true){
     h->array[index]=new;
     ++h->capacity;
   }
+  else if(listIndex != -1){
+      Node *node = getNode(h, listIndex, index);
+      node->data = ch;
+    }
   else{
     Node *nextNode = h->array[index]->next;
     h->array[index]->next = new;
     new->next = nextNode;
     ++h->capacity;
   }
+  if (HashFull(h)){
+    reHash(h);
+  }
+}
+
+void printHash(HashTable *h){
+  int i = 0;
+  for (i = 0; i < h->max; i++){
+    Node *temp = h->array[i];
+    if (temp == NULL){
+      printf("array[%d] has no elements\n", i);
+    }
+    else{
+      printf("array[%d] has elements-: ", i);
+      while (temp != NULL){
+        printf("value= %s\t", temp->data);
+        temp = temp->next;
+      }
+      printf("\n");
+    }
+  }
 }
 
 
-
 int main(){
-  HashTable *h = newHash(8);
-  char *c = malloc(6*sizeof(char));
-  c = "Johns\0";
-  insertHash(h, c);
+  HashTable *h = newHash();
+  printf("How many characters would you like to insert\n");
+  char line[100];
+  int size;
+  fgets(line,sizeof(line), stdin);
+  sscanf(line, "%d", &size);
+  for (int i = 0; i<size; i++){
+    printf("What character would you like to hash?\n");
+    fgets(line, 100, stdin);
+    insertHash(h, line);
+  }
+  printHash(h);
+  freeHash(h);
   return 0;
 }
--- C Code/Standard Coursework/hash.h
+++ C Code/Standard Coursework/hash.h
@@ -5,3 +5,6 @@ typedef struct Node Node;
 
 struct HashTable;
 typedef struct HashTable HashTable;
+
+void reHash(HashTable *h);
+void insertHash(HashTable *h, char *ch);
