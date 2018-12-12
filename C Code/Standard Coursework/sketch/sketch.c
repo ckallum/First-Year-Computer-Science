5586bfdab409176c0a850c478ad4a87c8506ed22
ckallum <in18536@bristol.ac.uk>
2018-12-12 23:32:40 +0000

    hi

--- C Code/Standard Coursework/sketch/sketch.c
+++ C Code/Standard Coursework/sketch/sketch.c
@@ -6,15 +6,16 @@
 
 struct state {
   int px, py, cx, cy;
-  display *d;
+  int currentOp;
+  int DT;
+  display *display;
   bool pen;
+  bool init;
 };
 typedef struct state state;
 
 int opcode(int n){
-  int op;
-  op = (n>>6);
-  return op;
+  return (n>>6);
 }
 
 int operand(int n){
@@ -26,21 +27,28 @@ int operand(int n){
   return op;
 }
 
+int increaseOp(state *s, int n){
+  return 0;
+}
+
 state *newState(){
   state *s = malloc(sizeof(state));
   s->px = 0;
   s->py = 0;
   s->cx = 0;
   s->cy = 0;
+  s->currentOp = 0;
+  s->DT = 0;
   s->pen = false;
+  s->init = false;
   return s;
 }
 
 
-void dy(display *d, state *s, int n){
+void dy(state *s, int n){
   s->cy = s->cy+n;
   if (s->pen == true){
-    line(d, s->px, s->py, s->cx, s->cy);
+    line(s->display, s->px, s->py, s->cx, s->cy);
   }
   s->px = s->cx;
   s->py = s->cy;
@@ -54,27 +62,28 @@ void dx(state *s, int n){
 
 
 
-void update(display *d, int n, state *s){
+void update(int n, state *s){
   int opc = opcode(n);
   int oper = operand(n);
   printf("%d, %d\n", opc, oper);
+  if (opc == 2){};
   if (opc == 0) dx(s, oper);
-  else if (opc == 1) dy(d, s, oper);
+  else if (opc == 1) dy(s, oper);
   else if (opc == 3 && s->pen ==false) s->pen = true;
   else s->pen = false;
 
 }
 
-void initialise(display *d, int n, state *s){
-  update(d, n, s);
+void initialise(int n, state *s){
+  update(n, s);
 }
 
 
-void readFile(display *d, char inputfile[], state *s){
+void readFile (char inputfile[], state *s){
   FILE *in = fopen(inputfile, "rb");
   unsigned int n = fgetc(in);
   while (! feof(in)) {
-    initialise(d, n, s);
+    initialise(n, s);
     n = fgetc(in);
   }
   fclose(in);
@@ -87,7 +96,8 @@ int main(int n, char *args[n]){
   }
     state *s = newState();
     display *d = newDisplay(args[1], 200, 200);
-    readFile(d, args[1], s);
+    s->display = d;
+    readFile(args[1], s);
     end(d);
     free(s);
     return 0;
