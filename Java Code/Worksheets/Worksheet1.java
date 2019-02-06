aa71bef455bcc75340567d833a1e98ff9e9161a4
ckallum <ckallumke@gmail.com>
2019-02-06 09:33:08 +0000

    hi

---
+++ Java Code/Worksheets/Worksheet1.java
@@ -0,0 +1,5 @@
+//1a)Boolen functions determining function keys, control keys etc.
+//1b)All function keys, macro's etc.
+//2a)public = The class is shared can be seen outside the class. eg the Hello World class and main method can be accessed by all other classes
+//2b)This is so the JVM instantiates/calls the main method during run time first without instantiating any objects.Static means that an object doesn't have to be instantiated eg, a pure function
+//3a)
