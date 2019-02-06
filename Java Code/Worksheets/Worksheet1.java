3639787aa2285a27fa931ae8b66260d7c4a0ddec
ckallum <ckallumke@gmail.com>
2019-02-06 09:35:31 +0000

    hi

--- Java Code/Worksheets/Worksheet1.java
+++ Java Code/Worksheets/Worksheet1.java
@@ -3,3 +3,6 @@
 //2a)public = The class is shared can be seen outside the class. eg the Hello World class and main method can be accessed by all other classes
 //2b)This is so the JVM instantiates/calls the main method during run time first without instantiating any objects.Static means that an object doesn't have to be instantiated eg, a pure function
 //3a)
+public class Critter{
+
+}
