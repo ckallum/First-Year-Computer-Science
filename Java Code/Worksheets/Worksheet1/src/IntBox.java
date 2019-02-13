6077a9500eb28f3c04cf1abfad825f52c9662a99
ckallum <ckallumke@gmail.com>
2019-02-13 10:37:07 +0000

    hi

--- Java Code/Worksheets/Worksheet1/src/IntBox.java
+++ Java Code/Worksheets/Worksheet1/src/IntBox.java
@@ -1,29 +1,46 @@
-public class IntBox{
+class IntBox{
     int x;
 
-    public IntBox(int x) {
+    IntBox(int x) {
         this.x = x;
     }
 
+    IntBox(IntBox v){
+        this.x = v.x;
+    }
+
+
     IntBox add(int v){
-        IntBox newInt = new IntBox(this.x + v);
-        return newInt;
+        this.x += v;
+        return this;
     }
 
     IntBox subtract(int v) {
-        IntBox newInt = new IntBox(this.x - v);
-        return newInt;
+        this.x -=v;
+        return this;
     }
 
     IntBox scale(int v){
-        IntBox newInt = new IntBox(this.x * v);
-        return newInt;
+        this.x *= v;
+        return this;
     }
 
     @Override
-    public string toString(){
+    public String toString(){
         String string = "IntBox("+this.x+")";
-
+        return string;
     }
 }
+
+class Run {
+    public static void main(String[] args) {
+        IntBox a = new IntBox(10).add(10);
+        System.out.println(a.toString());
+        System.out.println(a.scale(10).toString());
+        System.out.println(a.toString());
+        IntBox b = new IntBox(a);
+        System.out.println(b.toString());
+    }
+}
+
 
