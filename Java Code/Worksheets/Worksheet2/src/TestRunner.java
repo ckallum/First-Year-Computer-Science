5ee01df762766909f66f7e14bb3c0be52efc7d5b
ckallum <ckallumke@gmail.com>
2019-03-02 20:37:12 +0000

    hi

--- Java Code/Worksheets/Worksheet2/src/TestRunner.java
+++ Java Code/Worksheets/Worksheet2/src/TestRunner.java
@@ -1,2 +1,16 @@
 public class TestRunner {
+    public static void main(String[] args) {
+        String a = new String("ab");
+        String b = new String("ab");
+        int aa = new Integer(34);
+        int bb = new Integer(34);
+//        TestUtils.assertEquals(a ,b);
+//        TestUtils.assertEquals(aa,bb);
+//        TestUtils.assertUniversalEquals(1, 2);
+//        TestUtils.assertUniversalEquals("1", "2");
+//        TestUtils.assertUniversalEquals(a,b);
+//        TestUtils.assertUniversalEquals("1", null);
+        TestUtils.assertUniversalEquals(1, "1");
+//        TestUtils.assertUniversalEquals(null, null);
+    }
 }
--- Java Code/Worksheets/Worksheet2/src/TestUtils.java
+++ Java Code/Worksheets/Worksheet2/src/TestUtils.java
@@ -1,2 +1,55 @@
 public class TestUtils {
+
+    static void assertEquals(String a, String b){
+        try {
+            if (a.equals(b)) {
+                System.out.println("Equal");
+            } else {
+                throw (new AssertionError("Expected string: " + a + " but was " + b));
+            }
+
+        }
+        catch (Exception e){
+            System.out.println("Exception:"+ e.getMessage());
+        }
+    }
+
+    static void assertEquals(int a, int b){
+        try {
+            if (a==b) {
+                System.out.println("Equal");
+            } else {
+                throw (new AssertionError("Expected int: " + a + " but was " + b));
+            }
+
+        }
+        catch (Exception e){
+            System.out.println("Exception:"+ e.getMessage());
+        }
+    }
+
+    static void assertUniversalEquals(Object a, Object b){
+        try{
+            if (a == null && b == null){
+                 System.out.println("OK");
+            }
+            else if(a == null){
+                throw (new AssertionError("Expected: null" +  "but got "+b+" ("+b.getClass()+ ")"));
+
+            }
+            else if (b == null){
+                throw (new AssertionError("Expected:" + a + " ("+a.getClass() + ") but got null"));
+
+            }
+
+            else if (!a.equals(b)){
+                throw (new AssertionError("Expected:" + a + " ("+a.getClass() + ") but got "+b+" ("+b.getClass()+ ")"));
+
+            }
+        }
+        catch (Exception e){
+            System.out.println(("Exception:" + e.getMessage()));
+        }
+
+    }
 }
